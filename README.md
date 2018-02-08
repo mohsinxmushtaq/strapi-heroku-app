044389c2aeeb263190d5f9fa08109d171c090f52
Pierre Burgy <pierre.burgy@gmail.com>
2018-02-08 11:54:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<a href="https://heroku.com/deploy">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
+</a>
+
+# Strapi Heroku App
+
+A Strapi app ready to be deployed on Heroku.
---
+++ config/functions/bootstrap.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid/v4');
+
+/**
+ * An asynchronous bootstrap function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ */
+
+module.exports = cb => {
+  try {
+    // Read the package.json
+    const pkgPath = path.resolve(process.cwd(), 'package.json');
+    const pkg = JSON.parse(fs.readFileSync(pkgPath));
+
+    // Default `strapi` value
+    pkg.strapi = pkg.strapi || {};
+
+    // If the `strapi.uuid is missing`, we add it in the package.json
+    if (!pkg.strapi.uuid) {
+      // Set the `uuid` value
+      pkg.strapi.uuid = `${uuid()}-heroku`;
+
+      // Overwrite the file
+      fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+
+      // Set value in config
+      strapi.config.uuid = pkg.strapi.uuid;
+    }
+  } catch (err) {
+    // Silent
+  }
+
+  cb();
+};
